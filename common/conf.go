41e59cb118db22be469f9e6ba29a14efe98cec5d
王健 <wangjian@wangjiandeMacBook-Pro.local>
2016-06-16 18:35:55 +0800

    move command line arguments into config file

--- common/conf.go
+++ common/conf.go
@@ -13,6 +13,43 @@ type ProxyConf struct {
 	ModuleConfPath string
 }
 
+type ProxyModuleConf struct {
+	PidFile        string
+	GlogDir        string
+	ModuleLogLevel int32
+	ProxyConfList  []ProxyConf
+}
+
+const (
+	ProxyModuleConfFile = "proxymodule.conf"
+)
+
+func LoadProxyModuleConfFromFile(confPath string) (ProxyModuleConf, error) {
+	var c ProxyModuleConf
+	confFile := filepath.Join(confPath, ProxyModuleConfFile)
+	bytes, err := ioutil.ReadFile(confFile)
+	if err != nil {
+		return c, err
+	}
+	if len(bytes) == 0 {
+		return c, nil
+	}
+	err = json.Unmarshal(bytes, &c)
+	return c, err
+}
+
+func LoadProxyModuleConfFromServer(serverKey string) (ProxyModuleConf, error) {
+	var c ProxyModuleConf
+	var bytes []byte
+	// TODO: load proxy configure from configure server.
+	if len(bytes) == 0 {
+		return c, nil
+	}
+	err := json.Unmarshal(bytes, &c)
+	return c, err
+}
+
+/*
 func LoadFromFile(fileName string) ([]ProxyConf, error) {
 	var c []ProxyConf
 	bytes, err := ioutil.ReadFile(fileName)
@@ -44,7 +81,7 @@ func LoadFromConfigServer(serverKey string) ([]ProxyConf, error) {
 		e.ModuleConfPath = serverKey + "_module_conf/" + e.ModuleConfPath
 	}
 	return c, err
-}
+}*/
 
 func LoadModuleConfFromFile(fileName string, v interface{}) error {
 	bytes, err := ioutil.ReadFile(fileName)
