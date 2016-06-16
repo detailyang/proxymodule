6116dbce5d7e259e70ef2998b09b1e5fee1787cb
王健 <wangjian@wangjiandeMacBook-Pro.local>
2016-06-16 19:10:08 +0800

    move command line arguments into config file

--- common/conf.go
+++ common/conf.go
@@ -35,6 +35,9 @@ func LoadProxyModuleConfFromFile(confPath string) (ProxyModuleConf, error) {
 		return c, nil
 	}
 	err = json.Unmarshal(bytes, &c)
+	for _, e := range c.ProxyConfList {
+		e.ModuleConfPath = filepath.Join(confPath, e.ModuleConfPath)
+	}
 	return c, err
 }
 
@@ -42,46 +45,15 @@ func LoadProxyModuleConfFromServer(serverKey string) (ProxyModuleConf, error) {
 	var c ProxyModuleConf
 	var bytes []byte
 	// TODO: load proxy configure from configure server.
-	if len(bytes) == 0 {
-		return c, nil
-	}
-	err := json.Unmarshal(bytes, &c)
-	return c, err
-}
-
-/*
-func LoadFromFile(fileName string) ([]ProxyConf, error) {
-	var c []ProxyConf
-	bytes, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return c, err
-	}
-	if len(bytes) == 0 {
-		return nil, nil
-	}
-	err = json.Unmarshal(bytes, &c)
-	if err != nil {
-		return c, err
-	}
-	for _, e := range c {
-		e.ModuleConfPath = filepath.Join(filepath.Dir(fileName), e.ModuleConfPath)
-	}
-	return c, err
-}
-
-func LoadFromConfigServer(serverKey string) ([]ProxyConf, error) {
-	var c []ProxyConf
-	var bytes []byte
-	// TODO: load proxy configure from configure server.
 	err := json.Unmarshal(bytes, &c)
 	if err != nil {
 		return c, err
 	}
-	for _, e := range c {
+	for _, e := range c.ProxyConfList {
 		e.ModuleConfPath = serverKey + "_module_conf/" + e.ModuleConfPath
 	}
 	return c, err
-}*/
+}
 
 func LoadModuleConfFromFile(fileName string, v interface{}) error {
 	bytes, err := ioutil.ReadFile(fileName)
