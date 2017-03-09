159c39d31f624ee7f1b471b073f54db147203fb2
wangjian-pg <wangjian.pg@gmail.com>
2017-03-09 16:32:51 +0800

    add statistics module for codis-proxy

--- redisproxy/redis_codis_module.go
+++ redisproxy/redis_codis_module.go
@@ -1,11 +1,13 @@
 package redisproxy
 
 import (
+	"encoding/json"
 	"errors"
 	"sync"
 	"sync/atomic"
 	"time"
 
+	"github.com/absolute8511/proxymodule/common"
 	"github.com/garyburd/redigo/redis"
 )
 
@@ -15,6 +17,9 @@ var (
 
 const (
 	defaultTendInterval = 3
+
+	cpMonitorApp      = "tether"
+	cpMonitorBusiness = "tether.codis-proxy"
 )
 
 type CodisServer struct {
@@ -28,12 +33,13 @@ type CodisProxyConf struct {
 
 type CodisProxy struct {
 	sync.Mutex
-	conf           *CodisProxyConf
-	connPool       *redis.Pool
-	ServerList     []CodisServer
-	quitC          chan struct{}
-	wg             sync.WaitGroup
-	tendServerList []CodisServer //buffer used in method 'tendServer'
+	conf             *CodisProxyConf
+	connPool         *redis.Pool
+	ServerList       []CodisServer
+	quitC            chan struct{}
+	wg               sync.WaitGroup
+	tendServerList   []CodisServer //buffer used in method 'tendServer'
+	statisticsModule *CodisStatisticsModule
 }
 
 func init() {
@@ -59,12 +65,17 @@ func init() {
 
 func CreateCodisProxy() RedisProxyModule {
 	return &CodisProxy{
-		quitC: make(chan struct{}),
+		quitC:            make(chan struct{}),
+		statisticsModule: NewCodisStatisticsModule(),
 	}
 }
 
 func (proxy *CodisProxy) GetProxyName() string {
 	return "codis-proxy"
+}
+
+func (proxy *CodisProxy) SupportedCommands() []string {
+	return supportedCommands
 }
 
 func (proxy *CodisProxy) InitConf(loadConfig func(v interface{}) error) error {
@@ -135,6 +146,8 @@ func (proxy *CodisProxy) RegisterCmd(router *CmdRouter) {
 			cmdArgs[i] = v
 		}
 
+		proxy.statisticsModule.Sampling(c.cmd)
+
 		if reply, err := conn.Do(c.cmd, cmdArgs...); err != nil {
 			return err
 		} else {
@@ -149,7 +162,7 @@ func (proxy *CodisProxy) RegisterCmd(router *CmdRouter) {
 }
 
 func (proxy *CodisProxy) GetStatisticsModule() ProxyStatisticsModule {
-	return nil
+	return proxy.statisticsModule
 }
 
 func (proxy *CodisProxy) tend() {
@@ -202,4 +215,112 @@ func (proxy *CodisProxy) Stop() {
 	close(proxy.quitC)
 	proxy.connPool.Close()
 	proxy.wg.Wait()
+}
+
+func NewCodisStatisticsModule() *CodisStatisticsModule {
+	return &CodisStatisticsModule{
+		statisticsData: make(map[string]uint64),
+		swapData:       make(map[string]uint64),
+		slowOperation:  make(map[string]uint64),
+	}
+}
+
+type CodisStatisticsModule struct {
+	sync.Mutex
+	statisticsData map[string]uint64
+	swapData       map[string]uint64
+
+	accumulatedOpTime int64
+	failedOperation   uint64
+	slowOperation     map[string]uint64
+}
+
+func (self *CodisStatisticsModule) IncrOpTime(cost int64) {
+	atomic.AddInt64(&self.accumulatedOpTime, cost)
+}
+
+func (self *CodisStatisticsModule) IncrFailedOperation() {
+	atomic.AddUint64(&self.failedOperation, 1)
+}
+
+func (self *CodisStatisticsModule) IncrSlowOperation(cost time.Duration) {
+	defer self.Mutex.Unlock()
+	self.Mutex.Lock()
+
+	switch {
+	case cost > 20*time.Millisecond:
+		self.slowOperation["20ms"] += 1
+		fallthrough
+	case cost > 10*time.Millisecond:
+		self.slowOperation["10ms"] += 1
+		fallthrough
+	case cost > 5*time.Millisecond:
+		self.slowOperation["5ms"] += 1
+		fallthrough
+	case cost > 2*time.Millisecond:
+		self.slowOperation["2ms"] += 1
+	default:
+		return
+	}
+}
+
+func (self *CodisStatisticsModule) Sampling(Cmd string) {
+	self.Mutex.Lock()
+	self.statisticsData[Cmd] += 1
+	self.Mutex.Unlock()
+}
+
+func (self *CodisStatisticsModule) GenMonitorData() []byte {
+	self.Mutex.Lock()
+	tmp := self.statisticsData
+	self.statisticsData = self.swapData
+	self.swapData = tmp
+	self.Mutex.Unlock()
+
+	monitorData := make([]*common.MonitorData, 0, len(self.swapData)+1)
+
+	var total uint64
+	for operation, count := range self.swapData {
+		opSample := common.NewMonitorData(cpMonitorApp, cpMonitorBusiness)
+		opSample.Metrics["OpCount"] = count
+		opSample.Tags["Operation"] = operation
+
+		monitorData = append(monitorData, opSample)
+		total += count
+
+		//clear the already stored data
+		self.swapData[operation] = 0
+	}
+
+	overall := common.NewMonitorData(cpMonitorApp, cpMonitorBusiness)
+	overall.Metrics["Total"] = total
+
+	costTime := uint64(atomic.SwapInt64(&self.accumulatedOpTime, 0)) / 1000
+	var avgCost uint64
+	if total != 0 {
+		avgCost = uint64(costTime / total)
+	}
+	overall.Metrics["Avg"] = avgCost
+
+	overall.Metrics["Failed"] = atomic.SwapUint64(&self.failedOperation, 0)
+
+	monitorData = append(monitorData, overall)
+
+	self.Mutex.Lock()
+	for datum, count := range self.slowOperation {
+		slowSample := common.NewMonitorData(cpMonitorApp, cpMonitorBusiness)
+		slowSample.Metrics["SlowCount"] = count
+		slowSample.Tags["datum"] = datum
+		monitorData = append(monitorData, slowSample)
+
+		//clear the already stored data
+		self.slowOperation[datum] = 0
+	}
+	self.Mutex.Unlock()
+
+	if monitorBytes, err := json.Marshal(monitorData); err != nil {
+		return nil
+	} else {
+		return monitorBytes
+	}
 }
